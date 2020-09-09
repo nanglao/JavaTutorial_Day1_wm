d1e8e4b28b0c20337bab6f52acd1ee44d0e92396
Nang Lao Khay <31026836+nanglao@users.noreply.github.com>
2020-09-10 00:09:24 +0900

    textfileからcsv fileに変更Java c1c3ae5

--- TxtConvertToCSV.java
+++ TxtConvertToCSV.java
@@ -1,79 +1,110 @@
+import java.io.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
+
 
 public class TxtConvertToCSV {
 	
-	public static void main(String[] args) {
-		
+	public static String filePath_koteicho = "/Users/Shared/convertCSV/input/労働者名簿_固定長.txt";
+	public static String filePath__fileLayout = "/Users/Shared/convertCSV/input/file_layout.txt";
+	public static String filePath__fileLayout1 = "/Users/Shared/convertCSV/input/file_layout.txt";
+	public static String filePath__CSVfile = "/Users/Shared/convertCSV/output/労働者名簿_固定長NAN.csv";
+	public static ArrayList<String> arrayString;
+	private static String outputFile;
+	public static StringBuilder sBuilder;
+	static String[] filterd;
+	 
+	
+	public static void main(String[] args) throws IOException {
 		//file_layout.txtからデータを取得
-		FileRead fileReadIndex = new FileRead();
-		fileReadIndex.fileRead();
-	    
-	    String filePath = "/Users/Shared/convertCSV/input/労働者名簿_固定長.txt";
-	    if (Files.exists(Paths.get(filePath))) {
-	      try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
-	        stream.forEach(line -> {
-	            
-	        	// regex patternを使って文字列を空白区切りで配列に変換する
-	            String pattern = "^(.{0,5})(.{0,30})(.{0,1})(.{0,8})(.{0,8})(.{0,10})(.{0,11})(.{0,15})(.{0,15})(.{0,12})$";
-
-	            // Pattern objectを作成
-	            Pattern r = Pattern.compile(pattern);
-
-	            // matcher objectを作成
-	            Matcher m = r.matcher(line);
-	            if (m.find()) {
-	                
-	                String[] filterd = {
-		            		m.group(1),
-		            		m.group(2),
-		            		m.group(3),
-		            		m.group(4),
-		            		m.group(5),
-		            		m.group(6),
-		            		m.group(7),
-		            		m.group(8),
-		            		m.group(9),
-		            		m.group(10),
-		            		};
-		            
-		            // 文字列配列をカンマ区切りの文字列に変換する
-		            String csvRow = String.join(",", filterd);
-		            System.out.println(csvRow);
-		            
-		            // CSV ファアリ出力ため
-		            File filePathOutput = new File("/Users/Shared/convertCSV/output/労働者名簿_固定長NAN.csv");	            
-		            PrintWriter pw = null;
-					try {
-						pw = new PrintWriter(filePathOutput);
-					} catch (FileNotFoundException e) {
-						e.printStackTrace();
-					}
-					int data ;
-					StringBuilder sb = new StringBuilder();
-					sb.append(csvRow);
-					pw.write(sb.toString());
-					pw.close();  
-		           		            
-	            } else {
-	                System.out.println("NO MATCH");
-	            }
-
-	        });
-	      } catch (IOException e) {
-	        e.printStackTrace();
-	      }
-	    } else {
-	      System.out.print("ファイルは存在しません");
-	    }
+		fileRead (filePath__fileLayout);
+		fileRead (filePath_koteicho);
+		outputFile = fileRead(filePath_koteicho);
+		fileWrite();
 	  }
-
+	
+  private static void fileWrite() throws IOException {
+        // CSV ファアリ出力ため
+	try {
+        File filePathOutput = new File("/Users/Shared/convertCSV/output/労働者名簿_固定長NAN.csv");
+	        if(!filePathOutput.exists()) {
+	        	filePathOutput.createNewFile();
+	        }
+	            if (filePathOutput.exists() && filePathOutput.canWrite()){
+	              FileWriter fw = new FileWriter(filePathOutput);
+				  System.out.print("sdsdsdsds"+ outputFile);
+	              fw.write(outputFile);
+	              fw.close();
+	            } else {
+	            	System.out.print("ファイルは書きこめません\n");
+	            }
+        }catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}           
+		
+}	
+	//FileReaderクラス
+	private static String fileRead(String readFileName) throws IOException {
+		
+			 try {
+				  File file = new File(readFileName);
+				      //ファイルが存在するか確認する
+						System.out.print("\n");
+						//FileReaderクラスのオブジェクトを生成する
+						FileReader filereader = new FileReader(file);
+						BufferedReader br = new BufferedReader(filereader); 
+						sBuilder = new StringBuilder();     
+						String data = null;
+		                while((data = br.readLine()) != null) {
+			               	  
+			               String str = data;
+			               String[] arrayList = str.toString().split( "," , 0 );
+				               for(int i =0;i < arrayList.length;i++) {
+			            	   System.out.println(arrayList[i]);
+				               }
+			            	   	String pattern = "^(.{0,5})(.{0,30})(.{0,1})(.{0,8})(.{0,8})(.{0,10})(.{0,11})(.{0,15})(.{0,15})(.{0,12})$";
+		            	   		Pattern r = Pattern.compile(pattern);
+		            	   		Matcher m = r.matcher(data);
+		            	   		if (m.find()) {
+		        	                String[] filterd = {
+		        		            		m.group(1),
+		        		            		m.group(2),
+		        		            		m.group(3),
+		        		            		m.group(4),
+		        		            		m.group(5),
+		        		            		m.group(6),
+		        		            		m.group(7),
+		        		            		m.group(8),
+		        		            		m.group(9),
+		        		            		m.group(10),
+		        		            		};
+		        		            
+		        		            // 文字列配列をカンマ区切りの文字列に変換する
+		        	                data = String.join(",", filterd);  
+			            	  
+				             }     
+		            	   		//String csvRow = String.join(",", filterd);
+		        		           sBuilder.append(data);
+		        		           sBuilder.append("\n");
+		        		           //Builder.toString();
+		        		           //sBuilder.append("\n");
+		        		           
+		               } 
+		             filereader.close();  
+				} catch (FileNotFoundException e) {
+					System.out.println(e);
+				} catch (IOException e) {
+					System.out.println(e);
+				}
+			 
+			 return sBuilder.toString();
+		}	
+	
 }
